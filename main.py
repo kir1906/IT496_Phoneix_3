d29a980ed4edea22a3575c533eb0f5bea48eac84
Het Patel <hetpatel3245@gmail.com>
2023-11-20 00:34:23 +0530

    API updated

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI
 import numpy as np
 import pickle 
 import pandas as pd
-from tensorflow import keras
+# from tensorflow import keras
+# from tensorflow.keras import models
 from player import player
+from player import totalRun
 from sklearn.preprocessing import StandardScaler
+
+
 
 app = FastAPI()
 # total_run_model = keras.models.load_model('./models/total_run_prediction.h5')
@@ -16,8 +20,11 @@ app = FastAPI()
 # players_model = pickle.load(pickle_model)
 
 
-pickle_model = open('players2.pkl', 'rb')
+pickle_model = open("players2.pkl", "rb")
 players_model = pickle.load(pickle_model)
+
+pickle_model1 = open("total_run_prediction.pkl", "rb")
+total_run_prediction_model = pickle.load(pickle_model1)
 
 @app.post('/player')
 def predict(data:player):
@@ -37,6 +44,50 @@ def predict(data:player):
     return {
         'prediction': prediction.tolist()
     }
+
+@app.post('/totalrun')
+def predict(data:totalRun):
+    #convert data obj to dictionary
+    data=dict(data)
+    Mat=data['Mat']
+    Inns=data['Inns']
+    Afghanistan=data['Afghanistan']
+    Australia=data['Australia']
+    Bangladesh=data['Bangladesh']
+    England=data['England']
+    India=data['India']
+    Netherlands=data['Netherlands']
+    New_Zealand=data['New_Zealand']
+    Pakistan=data['Pakistan']
+    South_Africa=data['South_Africa']
+    Sri_Lanka=data['Sri_Lanka']
+    AVG_Run=data['AVG_Run']
+    AVG_SR=data['AVG_SR']
+    AVG_BF=data['AVG_BF']
+    Player_pre=data['Player_pre']
+    Team_pre=data['Team_pre']
+    prediction = total_run_prediction_model.predict([[ Mat,
+    Inns, 
+    Afghanistan,
+    Australia,
+    Bangladesh,
+    England,
+    India,
+    Netherlands,
+    New_Zealand,
+    Pakistan,
+    South_Africa,
+    Sri_Lanka,
+    AVG_Run,
+    AVG_SR,
+    AVG_BF,
+    Player_pre,
+    Team_pre]])
+    # prediction = players_model.predict([[-1.41234016,  0.19088097, -0.80355172,  0.17038773,  0.12463426,0.17287699, -1.03077641, -1.10049766, -1.03077641]])
+    return {
+        'prediction': prediction.tolist()
+    }
+
 @app.get('/')
 def index():
     return {'Hello, this is the project of Group 1: Team Phoenix'}
--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class player(BaseModel):
     Name: float 
@@ -11,7 +11,33 @@ class player(BaseModel):
     Roll : float
     Team : float
 
+class totalRun(BaseModel):
+    Mat: float 
+    Inns: float 
+    Afghanistan: float 
+    Australia: float
+    Bangladesh: float
+    England: float
+    India: float
+    Netherlands: float
+    New_Zealand: float = Field(..., alias='New Zealand')
+    Pakistan: float
+    South_Africa: float = Field(..., alias='South Africa')
+    Sri_Lanka: float = Field(..., alias='Sri Lanka')
+    AVG_Run: float
+    AVG_SR: float
+    AVG_BF: float
+    Player_pre: float
+    Team_pre: float
 
+
+class Config:
+        fields = {
+            'New Zealand': 'new_zealand',
+            'South Africa': 'south_africa',
+            'Sri Lanka': 'sri_lanka',
+        }
+#player
 # {
 #   "Name": -1.41234016,
 #   "bat_score": 0.19088097,
@@ -22,4 +48,25 @@ class player(BaseModel):
 #   "Oppsition": -1.03077641,
 #   "Roll": -1.10049766,
 #   "Team": -1.03077641
+# }
+
+#totalrun
+# {
+#     "Mat": 1.95815139, 
+#     "Inns": 2.44437371, 
+#     "Afghanistan": 1.74507384, 
+#     "Australia": 0.66948347,
+#     "Bangladesh": 1.10318461,
+#     "England" : 0.61029341,
+#     "India" : 0.,
+#     "Netherlands" : 1.98640736,
+#     "New Zealand": 0.71761757,
+#     "Pakistan": 0.76641804, 
+#     "South Africa" : 0.72984558, 
+#     "Sri Lanka" : 0.58558649,
+#     "AVG_Run" : 2.10224609,
+#     "AVG_SR" : 0.22749678, 
+#     "AVG_BF" : 1.78218302,
+#     "Player_pre"	 : 1.66242976, 
+#     "Team_pre" : 1.43902213
 # }
