0d5d1821ca213cca88bc8d35c21dc3df12f0bacb
dhyeyladani24 <202101182@daiict.ac.in>
2023-11-23 22:40:29 +0530

    API_most_fours_prediction

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import numpy as np
 import pickle 
 import pandas as pd
 from tensorflow import keras
-from player import player, totalRun
+from player import player, totalRun,mostfours
 from sklearn.preprocessing import StandardScaler
 
 app = FastAPI()
 
-# most_fours_model = keras.models.load_model('./models/most_fours.h5')
 # finalists_model = keras.models.load_model('./models/Final_Model.keras')
-
 
 
 pickle_model = open("players2.pkl", "rb")
 players_model = pickle.load(pickle_model)
 
 total_run_prediction_model = keras.models.load_model('./models/total_run_prediction.h5')
+most_fours_model = keras.models.load_model('./most_fours.h5')
 
 @app.post('/player')
 def predict(data:player):
@@ -71,6 +70,49 @@ def predict(data:totalRun):
     Pakistan,
     SouthAfrica,
     SriLanka,
+    AVG_Run,
+    AVG_SR,
+    AVG_BF,
+    Player_pre,
+    Team_pre]])
+   
+    return {
+        'prediction': prediction.tolist()
+    }
+
+@app.post('/most_fours')
+def predict(data:mostfours):
+    #convert data obj to dictionary
+    data=dict(data)
+    Mat=data['Mat']
+    Inns=data['Inns']
+    Afghanistan=data['Afghanistan']
+    Australia=data['Australia']
+    Bangladesh=data['Bangladesh']
+    England=data['England']
+    India=data['India']
+    Netherlands=data['Netherlands']
+    New_Zealand=data['New_Zealand']
+    Pakistan=data['Pakistan']
+    South_Africa=data['South_Africa']
+    Sri_Lanka=data['Sri_Lanka']
+    AVG_Run=data['AVG_Run']
+    AVG_SR=data['AVG_SR']
+    AVG_BF=data['AVG_BF']
+    Player_pre=data['Player_pre']
+    Team_pre=data['Team_pre']
+    prediction = most_fours_model.predict([[ Mat,
+    Inns, 
+    Afghanistan,
+    Australia,
+    Bangladesh,
+    England,
+    India,
+    Netherlands,
+    New_Zealand,
+    Pakistan,
+    South_Africa,
+    Sri_Lanka,
     AVG_Run,
     AVG_SR,
     AVG_BF,
