fdd00dea386ee243bd11e864133cf013f09bdb16
dhyeyladani24 <202101182@daiict.ac.in>
2023-11-23 22:40:55 +0530

    API_most_fours_prediction

--- player.py
+++ player.py
@@ -30,6 +30,24 @@ class totalRun(BaseModel):
     Player_pre: float
     Team_pre: float
 
+class mostfours(BaseModel):
+    Mat: float 
+    Inns: float 
+    Afghanistan: float 
+    Australia: float
+    Bangladesh: float
+    England: float
+    India: float
+    Netherlands: float
+    New_Zealand: float = Field(..., alias='New Zealand')
+    Pakistan: float
+    South_Africa: float = Field(..., alias='South Africa')
+    Sri_Lanka: float = Field(..., alias='Sri Lanka')
+    AVG_Run: float
+    AVG_SR: float
+    AVG_BF: float
+    Player_pre: float
+    Team_pre: float
 
 #player
 # {
@@ -58,6 +76,27 @@ class totalRun(BaseModel):
 #     "Pakistan": 0.76641804, 
 #     "SouthAfrica" : 0.72984558, 
 #     "SriLanka" : 0.58558649,
+#     "AVG_Run" : 2.10224609,
+#     "AVG_SR" : 0.22749678, 
+#     "AVG_BF" : 1.78218302,
+#     "Player_pre"	 : 1.66242976, 
+#     "Team_pre" : 1.43902213
+# }
+
+# mostfours
+# {
+#     "Mat": 1.95815139, 
+#     "Inns": 2.44437371, 
+#     "Afghanistan": 1.74507384, 
+#     "Australia": 0.66948347,
+#     "Bangladesh": 1.10318461,
+#     "England" : 0.61029341,
+#     "India" : 0,
+#     "Netherlands" : 1.98640736,
+#     "New Zealand": 0.71761757,
+#     "Pakistan": 0.76641804, 
+#     "South Africa" : 0.72984558, 
+#     "Sri Lanka" : 0.58558649,
 #     "AVG_Run" : 2.10224609,
 #     "AVG_SR" : 0.22749678, 
 #     "AVG_BF" : 1.78218302,
